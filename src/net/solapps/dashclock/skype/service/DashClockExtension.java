be321a37a93a724d0279bdc53779362d6ad5cf3a
Maksim Solodovnikov <solmaks@gmail.com>
2014-10-04 20:30:31 +0300

    Added DashClockExtension and NotificationListenerService extensions under net.solapps.dashclock.skype.service package for compatibility purposes.

---
+++ src/net/solapps/dashclock/skype/service/DashClockExtension.java
@@ -0,0 +1,5 @@
+package net.solapps.dashclock.skype.service;
+
+public class DashClockExtension extends
+		net.solapps.dashclock.notification.service.DashClockExtension {
+}
---
+++ src/net/solapps/dashclock/skype/service/NotificationListenerService.java
@@ -0,0 +1,5 @@
+package net.solapps.dashclock.skype.service;
+
+public class NotificationListenerService extends
+		net.solapps.dashclock.notification.service.NotificationListenerService {
+}
